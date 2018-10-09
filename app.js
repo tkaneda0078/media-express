4372b8d107fb29d56ea3d2b72540e79c8322a7db
tkaneda0078 <1111510078r21@gmail.com>
2018-10-10 07:45:46 +0900

    テンプレートエンジン設定

--- app.js
+++ app.js
@@ -3,12 +3,22 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const helmet = require('helmet')
+const path = require('path')
 
 const app = express()
+
+const index = require('./routes/index.controller')
 
 app.use(helmet())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+// routes engine setup
+app.use('/', index)
 
 app.listen(process.env.PORT || 3000, () =>
   console.log('Server connected'))
