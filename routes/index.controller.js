321f59fd0601d10bd1d86cd70e9a403d09ce3f73
tkaneda0078 <1111510078r21@gmail.com>
2018-10-29 07:40:30 +0900

    一覧ページ作成

--- routes/index.controller.js
+++ routes/index.controller.js
@@ -7,4 +7,9 @@ router.get('/', (req, res) => {
   res.render('index')
 })
 
+router.get('/category/:type', (req, res) => {
+  let type = req.params.type
+  res.render('list')
+})
+
 module.exports = router
