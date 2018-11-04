3641687cb1562a7937f27c971e6d9160bf485351
tkaneda0078 <1111510078r21@gmail.com>
2018-11-04 11:10:25 +0900

    記事クラス作成（DB接続未実装）

---
+++ models/article.js
@@ -0,0 +1,160 @@
+'use strict'
+
+class Article {
+
+  constructor () {
+    this.data = {}
+  }
+
+  /**
+   * 記事を取得する
+   */
+  getArticles () {
+    return this.data
+  }
+
+  /**
+   * トピック記事を設定する
+   *
+   * todo: DBからデータ取得
+   */
+  setTopicArticle () {
+    this.data['topicArticle'] = {
+      'img': '/images/sample2.jpg',
+      'summary': 'sample topic'
+    }
+  }
+
+  /**
+   * ランキング記事を設定する
+   *
+   * todo: DBからデータ取得
+   */
+  setRankingArticles () {
+    this.data['rankingArticles'] = [
+      {
+        'img': '/images/sample2.jpg',
+        'summary': 'sample new summay1'
+      },
+      {
+        'img': '/images/sample.jpg',
+        'summary': 'sample nwe summay2'
+      },
+      {
+        'img': '/images/sample.jpg',
+        'summary': 'sample new summay3'
+      },
+      {
+        'img': '/images/sample4.jpg',
+        'summary': 'sample new summay4'
+      },
+      {
+        'img': '/images/sample4.jpg',
+        'summary': 'sample new summay5'
+      },
+      {
+        'img': '/images/sample2.jpg',
+        'summary': 'sample new summay6'
+      },
+      {
+        'img': '/images/sample4.jpg',
+        'summary': 'sample new summay7'
+      },
+      {
+        'img': '/images/sample3.jpg',
+        'summary': 'sample new summay8'
+      },
+      {
+        'img': '/images/sample3.jpg',
+        'summary': 'sample new summay9'
+      },
+      {
+        'img': '/images/sample3.jpg',
+        'summary': 'sample new summay10'
+      },
+    ]
+  }
+
+  /**
+   * 最新の記事を設定する
+   *
+   * todo: DBからデータ取得
+   */
+  setNewArticles () {
+    this.data['newArticles'] = [
+      {
+        'img'    : '/images/sample2.jpg',
+        'summary': 'sample new summay1'
+      },
+      {
+        'img'    : '/images/sample.jpg',
+        'summary': 'sample nwe summay2'
+      },
+      {
+        'img'    : '/images/sample.jpg',
+        'summary': 'sample new summay3'
+      },
+      {
+        'img'    : '/images/sample4.jpg',
+        'summary': 'sample new summay4'
+      },
+      {
+        'img'    : '/images/sample4.jpg',
+        'summary': 'sample new summay5'
+      },
+      {
+        'img'    : '/images/sample2.jpg',
+        'summary': 'sample new summay6'
+      },
+      {
+        'img'    : '/images/sample4.jpg',
+        'summary': 'sample new summay7'
+      },
+      {
+        'img'    : '/images/sample3.jpg',
+        'summary': 'sample new summay8'
+      }
+    ]
+  }
+
+  /**
+   * 全カテゴリの記事を設定する
+   *
+   * todo: DBからデータ取得
+   */
+  setAllCategoryArticles () {
+    this.data['categoryArticles'] = {
+      'category1': [
+        {
+          'img'    : '/images/sample2.jpg',
+          'summary': 'sample category summay1'
+        },
+        {
+          'img'    : '/images/sample.jpg',
+          'summary': 'sample category summay2'
+        },
+        {
+          'img'    : '/images/sample4.jpg',
+          'summary': 'sample category summay3'
+        }
+      ],
+      'category2': [
+        {
+          'img'    : '/images/sample2.jpg',
+          'summary': 'sample category summay1'
+        },
+        {
+          'img'    : '/images/sample.jpg',
+          'summary': 'sample category summay2'
+        },
+        {
+          'img'    : '/images/sample4.jpg',
+          'summary': 'sample category summay3'
+        }
+      ],
+    }
+  }
+
+}
+
+module.exports = Article
--- routes/index.controller.js
+++ routes/index.controller.js
@@ -2,162 +2,42 @@
 
 const express = require('express')
 const router = express.Router()
+const Article = require('../models/article')
 
 router.get('/', (req, res) => {
-  // todo modelで取得する
-  let data = {}
+  const article = new Article()
 
-  let topic = {
-    'img'    : '/images/sample3.jpg',
-    'summary': 'sample summay'
-  }
+  article.setTopicArticle()
+  article.setRankingArticles()
+  article.setNewArticles()
+  article.setAllCategoryArticles()
 
-  data['topic'] = topic
-
-  let rankingArticles = [
-    {
-      'img'    : '/images/sample2.jpg',
-      'summary': 'sample new summay1'
-    },
-    {
-      'img'    : '/images/sample.jpg',
-      'summary': 'sample nwe summay2'
-    },
-    {
-      'img'    : '/images/sample.jpg',
-      'summary': 'sample new summay3'
-    },
-    {
-      'img'    : '/images/sample4.jpg',
-      'summary': 'sample new summay4'
-    },
-    {
-      'img'    : '/images/sample4.jpg',
-      'summary': 'sample new summay5'
-    },
-    {
-      'img'    : '/images/sample2.jpg',
-      'summary': 'sample new summay6'
-    },
-    {
-      'img'    : '/images/sample4.jpg',
-      'summary': 'sample new summay7'
-    },
-    {
-      'img'    : '/images/sample3.jpg',
-      'summary': 'sample new summay8'
-    },
-    {
-      'img'    : '/images/sample3.jpg',
-      'summary': 'sample new summay9'
-    },
-    {
-      'img'    : '/images/sample3.jpg',
-      'summary': 'sample new summay10'
-    },
-  ]
-
-  data['rankingArticles'] = rankingArticles
-
-  let newArticles = [
-    {
-      'img'    : '/images/sample2.jpg',
-      'summary': 'sample new summay1'
-    },
-    {
-      'img'    : '/images/sample.jpg',
-      'summary': 'sample nwe summay2'
-    },
-    {
-      'img'    : '/images/sample.jpg',
-      'summary': 'sample new summay3'
-    },
-    {
-      'img'    : '/images/sample4.jpg',
-      'summary': 'sample new summay4'
-    },
-    {
-      'img'    : '/images/sample4.jpg',
-      'summary': 'sample new summay5'
-    },
-    {
-      'img'    : '/images/sample2.jpg',
-      'summary': 'sample new summay6'
-    },
-    {
-      'img'    : '/images/sample4.jpg',
-      'summary': 'sample new summay7'
-    },
-    {
-      'img'    : '/images/sample3.jpg',
-      'summary': 'sample new summay8'
-    },
-  ]
-
-  data['newArticles'] = newArticles
-
-  let categoryArticles = [
-    {
-      'img'    : '/images/sample2.jpg',
-      'summary': 'sample category summay1'
-    },
-    {
-      'img'    : '/images/sample.jpg',
-      'summary': 'sample category summay2'
-    },
-    {
-      'img'    : '/images/sample4.jpg',
-      'summary': 'sample category summay3'
-    },
-  ]
-
-  data['categoryArticles'] = categoryArticles
-
-  res.render('index', data)
+  res.render('index', article.getArticles())
 })
 
 router.get('/category/:category', (req, res) => {
-  // todo カテゴリ別にmodelでデータ取得する
-  let data = {}
+  const article = new Article()
 
   let category = req.params.category
   data['category'] = category
 
-  let articles = [
-    {
-      'img'    : '/images/sample2.jpg',
-      'summary': 'sample category summay1'
-    },
-    {
-      'img'    : '/images/sample.jpg',
-      'summary': 'sample category summay2'
-    },
-    {
-      'img'    : '/images/sample4.jpg',
-      'summary': 'sample category summay3'
-    },
-  ]
+  article.setAllCategoryArticles()
 
-  data['articles'] = articles
-
-  res.render('list', data)
+  res.render('list', article.getArticles())
 })
 
 router.get('/article/:id', (req, res) => {
-  // todo カテゴリ別にmodelでデータ取得する
-  let data = {}
+  const article = new Article()
 
   let articleId = req.params.id
   data['articleId'] = articleId
 
-  let article = {
-    'img'    : '/images/sample2.jpg',
-    'title'  : 'article title'
-  }
+  // let article = {
+  //   'img'    : '/images/sample2.jpg',
+  //   'title'  : 'article title'
+  // }
 
-  data['article'] = article
-
-  res.render('detail', data)
+  res.render('detail', article.getArticles())
 })
 
 module.exports = router
